8227d8d42b1563db325083592b6bf0f0cdb2bb27
Omochice <mochice.mls.ntl@gmail.com>
2025-09-23 01:23:25 +0900

    fixup! wip: self config holder

--- lua/nvim-tree-docs/configure.lua
+++ lua/nvim-tree-docs/configure.lua
@@ -1,14 +1,27 @@
-local default_config = {}
-local config = {}
+local M = {}
 
 --- @alias SpecConfig { slots?: { [string]: boolean }, processors?: { [string]: fun(): string } }
---- @alias Option { spec_config?: { [string]: SpecConfig }}
+--- @alias LangConfig { [string]: SpecConfig }
+--- @alias Option {lang_config?: {[string]: LangConfig }, spec_config?: { [string]: SpecConfig },  disable_default_mappings: boolean }
 
+--- @type Option
+local config = {}
+
+--- @type Option
+local default_config = {
+  disable_default_mappings = false,
+}
+
+--- Set user config
 --- @param user_config Option
 function M.set(user_config)
   config = vim.tbl_deep_extend("force", default_config, user_config)
 end
 
+--- Get read-only config
+--- @return Option
 function M.get()
-  return config
+  return vim.deepcopy(config)
 end
+
+return M
--- lua/nvim-tree-docs/init.lua
+++ lua/nvim-tree-docs/init.lua
@@ -1,8 +1,40 @@
 local M = {}
 
---- @param opts Option
-function M.configure(opts)
-  require("nvim-tree-docs.configure").set(opts)
+local group = vim.api.nvim_create_augroup("nvim-tree-docs", { clear = true })
+
+local function is_supported(lang)
+  return require("nvim-treesitter.query").get_query(lang, "docs") ~= nil
+end
+
+--- @param opts? Option
+function M.setup(opts)
+  require("nvim-tree-docs.configure").set(opts or {})
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    callback = function(args)
+      require("nvim-tree-docs.internal").detach(args.buf)
+
+      local lang = vim.treesitter.language.get_lang(args.match)
+      if is_supported(lang) then
+        return
+      end
+      require("nvim-tree-docs.internal").attach(args.buf)
+    end,
+    group = group,
+  })
+
+  vim.api.nvim_create_autocmd({ "BufUnload" }, {
+    callback = function(args)
+      require("nvim-tree-docs.internal").detach(args.buf)
+    end,
+    group = group,
+  })
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  if not require("nvim-tree-docs.configure").get().disable_default_mappings and
+      is_supported(vim.bo[bufnr].filetype)
+  then
+    require("nvim-tree-docs.internal").attach(bufnr)
+  end
 end
 
 function M.doc_node_at_cursor()
