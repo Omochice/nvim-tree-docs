485c68d736aa0731ec9c5dbc51fd2c07fce3f64c
Omochice <mochice.mls.ntl@gmail.com>
2026-02-27 02:25:27 +0900

    test: add test for Go function with parameters
    GoDoc style generates only a description comment without @param tags,
    confirming the output is the same regardless of parameters.

--- test/e2e/go.test.lua
+++ test/e2e/go.test.lua
@@ -23,4 +23,23 @@ describe("go godoc", function()
       "}",
     }, vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
   end)
+  it("should generate godoc for function with parameters", function()
+    local contents = {
+      "func Sample(a int, b string) error {",
+      "\treturn nil",
+      "}",
+    }
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+    vim.treesitter.get_parser(bufnr, "go"):parse()
+    vim.api.nvim_win_set_cursor(0, { 1, 5 })
+
+    require("nvim-tree-docs").doc_node_at_cursor()
+
+    assert.same({
+      "// Sample description",
+      "func Sample(a int, b string) error {",
+      "\treturn nil",
+      "}",
+    }, vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
+  end)
 end)
